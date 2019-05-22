0d5914fa98dba241f183b149c7af5c4cb6a4990d
Mike Walmsley <walmsleymk1@gmail.com>
2019-05-22 19:26:18 +0100

    Drop duplicates!

--- gzreduction/panoptes/api/reformat_api_like_exports.py
+++ gzreduction/panoptes/api/reformat_api_like_exports.py
@@ -177,6 +177,9 @@ def derive_directories_with_spark(dirs, output_dir, workflows, workflow_id, mode
         df = spark.read.json(dirs, schema=schema)
 
     df = df.filter(df['links']['workflow'] == workflow_id)
+
+    df = df.dropDuplicates('id')  # classification id is unique. Unlike pandas, keeps one row.
+
     df = clarify_workflow_version(df)
     df = rename_metadata_like_exports(df)
 

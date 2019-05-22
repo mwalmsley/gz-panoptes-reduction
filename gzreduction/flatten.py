512c59cea8e44972b81d6df32d02877b21b26107
Mike Walmsley <walmsleymk1@gmail.com>
2019-05-22 13:02:13 +0100

    debugging

--- gzreduction/flatten.py
+++ gzreduction/flatten.py
@@ -38,19 +38,21 @@ get_answer_udf = udf(lambda x, y: get_answer(x, y, dr5_schema), returnType=Strin
 
 def api_df_to_responses(df):
 
-    print(df.writeStream
+    df.writeStream
     .format("memory")
     .queryName("tableName")
-    .start().count(), 'before filter')
+    .start()
+    )
+    print(df.count(), 'before filter')
 
     df = df.withColumn('created_at', to_timestamp(df['created_at']))
     start_date = to_date(lit('2018-03-20')).cast(TimestampType())  # lit means 'column of literal value' i.e. dummy column of that everywhere
     df = df.filter(df['created_at'] > start_date)
 
-    print(df.writeStream
-    .format("memory")
-    .queryName("tableName")
-    .start().count(), 'after filter')
+    # print(df.writeStream
+    # .format("memory")
+    # .queryName("tableName")
+    # .start().count(), 'after filter')
 
     exploded = df.select(
         explode('annotations').alias('annotations_struct'),
@@ -67,21 +69,21 @@ def api_df_to_responses(df):
         'classification_id'
         )
 
-    print(flattened.writeStream
-    .format("memory")
-    .queryName("tableName")
-    .start().count(), 'after explode')
+    # print(flattened.writeStream
+    # .format("memory")
+    # .queryName("tableName")
+    # .start().count(), 'after explode')
     # filter for multiple choice and None
     flattened = flattened.filter(flattened['multiple_choice'] == False) # can only compare without lit() when calling directly e.g. df[x]    
-    print(flattened.writeStream
-    .format("memory")
-    .queryName("tableName")
-    .start().count(), 'after mc')
+    # print(flattened.writeStream
+    # .format("memory")
+    # .queryName("tableName")
+    # .start().count(), 'after mc')
     flattened = flattened.filter(flattened['value'] != 'No')
-    print(flattened.writeStream
-    .format("memory")
-    .queryName("tableName")
-    .start().count(), 'after has value')
+    # print(flattened.writeStream
+    # .format("memory")
+    # .queryName("tableName")
+    # .start().count(), 'after has value')
 
     # https://docs.databricks.com/spark/latest/spark-sql/udf-python.html
     flattened = flattened.withColumn('cleaned_response', sanitise_string_udf(flattened['value']))
